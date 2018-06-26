2a3adc5b51d97840260c10a960a1cf8d202ee030
Joshua Klimaszewski <joshua@autobooks.co>
2018-06-26 14:13:00 -0400

    started header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,11 @@ import './style.css';
 class Header extends Component {
     render() {
         return (
-            <div>[header]</div>
+            <div>
+                <div>
+                    [autobooks customer]
+                </div>
+            </div>
         )
     };
 }
