86ab51fb2565204fbe62400326ce8bfe4cb69353
Joshua Klimaszewski <joshua@autobooks.co>
2018-06-26 13:43:39 -0400

    set up routing, header, nav, dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        [mock-ui]
+        [header]
+        [nav]
+        <BrowserRouter>
+          <div>
+            <Switch>
+              <Route path="/" component={Dashboard} />
+            </Switch>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class Nav extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        };
+    }
+    render() {
+        return (
+            <div>[Nav-Bar]</div>
+        )
+    }
+}
+export default Nav;
