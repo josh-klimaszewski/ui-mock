55e2b485739755fd92339b106ac367b094b71624
Joshua Klimaszewski <joshua@autobooks.co>
2018-06-26 13:15:38 -0400

    folder architecture, started middleware

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux-promise';
+// import reducers from './reducers';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
