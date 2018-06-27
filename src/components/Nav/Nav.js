4cef5d9f939af40fb8c9537232ff5f816d40fb35
Danny Emmerson <danny@autobooks.co>
2018-06-27 15:17:58 -0400

    navbarwork

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -14,34 +14,33 @@ class Nav extends Component {
             <div className="nav-bar">
                 <div className="links">
                     <div>
-                        <i className='material-icons'>dashboard</i>
-                        <Link to="/">Dashboard</Link>
+                        <i className='material-icons md-dark'>dashboard</i>
+                        <Link to="/" className='md-dark'>Dashboard</Link>
                     </div>
                     <div>
-                        <i className='material-icons'>receipt</i>
-                        <Link to="/invoices">Invoices</Link>
+                        <i className='material-icons md-dark md-inactive'>receipt</i>
+                        <Link to="/invoices" className='md-dark md-inactive'>Invoices</Link>
                     </div>
                     <div>
-                        <i className='material-icons'>credit_card</i>
-                        <Link to="/paybills">Pay Bills</Link>
+                        <i className='material-icons md-dark md-inactive'>credit_card</i>
+                        <Link to="/paybills" className='md-dark md-inactive'>Pay Bills</Link>
                     </div>
                     <div>
-                        <i className='material-icons'>assessment</i>
-                        <Link to="/accounting">Accounting</Link>
+                        <i className='material-icons md-dark md-inactive'>assessment</i>
+                        <Link to="/accounting" className='md-dark md-inactive'>Accounting</Link>
                     </div>
                     <div>
-                        <i className='material-icons'>subject</i>
-                        <Link to="/reports">Reports</Link>
+                        <i className='material-icons md-dark md-inactive'>subject</i>
+                        <Link to="/reports" className='md-dark md-inactive'>Reports</Link>
                     </div>
                     <div>
-                        <i className='material-icons'>device_hub</i>
-                        <Link to="/connectedapps">Connected Apps</Link>
+                        <i className='material-icons md-dark md-inactive'>device_hub</i>
+                        <Link to="/connectedapps" className='md-dark md-inactive'>Connected Apps</Link>
                     </div>
                 </div>
                 <div className="settings">
-                    <div>
-                        <Link to="/settings">Settings</Link>
-                    </div>
+                    <i className='material-icons md-dark md-inactive'>settings</i>
+                    <Link to="/settings" className='md-dark md-inactive'>Settings</Link>
                 </div>
             </div>
         )
