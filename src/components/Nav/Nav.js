efadb5a8b17a427ec01e96e30c17fc1fa934d9af
Danny Emmerson <danny@autobooks.co>
2018-06-27 11:52:26 -0400

    nav-icons

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -14,21 +14,27 @@ class Nav extends Component {
             <div className="nav-bar">
                 <div className="links">
                     <div>
+                        <i className='material-icons'>dashboard</i>
                         <Link to="/">Dashboard</Link>
                     </div>
                     <div>
+                        <i className='material-icons'>receipt</i>
                         <Link to="/invoices">Invoices</Link>
                     </div>
                     <div>
+                        <i className='material-icons'>credit_card</i>
                         <Link to="/paybills">Pay Bills</Link>
                     </div>
                     <div>
+                        <i className='material-icons'>assessment</i>
                         <Link to="/accounting">Accounting</Link>
                     </div>
                     <div>
+                        <i className='material-icons'>subject</i>
                         <Link to="/reports">Reports</Link>
                     </div>
                     <div>
+                        <i className='material-icons'>device_hub</i>
                         <Link to="/connectedapps">Connected Apps</Link>
                     </div>
                 </div>
