ceb52fc6cada822f05fe0c9bd5659bb52f726d10
Josh Klimaszewski <joshklimaszewski@Joshs-MacBook-Pro.local>
2018-06-29 11:30:14 -0400

    redid nav bar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -17,86 +17,61 @@ class Nav extends Component {
       <div className="nav-bar">
         
         <div className="links">
-            {/* <Link to="/" 
+            <Link to="/" 
             className={this.state.activeNav === "dashboard" ? "active" : ""}
             onClick={() => this.setActiveNav("dashboard")}
             >
-            <i className="material-icons md-dark">dashboard</i>
-              Dashboard
-            </Link> */}
-          <div
-            className={this.state.activeNav === "dashboard" ? "active" : ""}
-            onClick={() => this.setActiveNav("dashboard")}
-          >
-            <i className="material-icons md-dark">dashboard</i>
-            <Link to="/" className="md-dark">
+              <i className="material-icons md-dark">dashboard</i>
               Dashboard
             </Link>
-          </div>
-          <div
+            <Link to="/" 
             className={this.state.activeNav === "invoices" ? "active" : ""}
             onClick={() => this.setActiveNav("invoices")}
-          >
-            <i className="material-icons md-dark md-inactive">receipt</i>
-            <Link to="/invoices" className="md-dark md-inactive">
+            >
+              <i className="material-icons md-dark">receipt</i>
               Invoices
             </Link>
-          </div>
-          <div
+            <Link to="/" 
             className={this.state.activeNav === "paybills" ? "active" : ""}
             onClick={() => this.setActiveNav("paybills")}
-          >
-            <i className="material-icons md-dark md-inactive">credit_card</i>
-            <Link to="/paybills" className="md-dark md-inactive">
+            >
+              <i className="material-icons md-dark">credit_card</i>
               Pay Bills
             </Link>
-          </div>
-          <div
+            <Link to="/" 
             className={this.state.activeNav === "accounting" ? "active" : ""}
             onClick={() => this.setActiveNav("accounting")}
-          >
-            <i className="material-icons md-dark md-inactive">assessment</i>
-            <Link to="/accounting" className="md-dark md-inactive">
+            >
+              <i className="material-icons md-dark">assessment</i>
               Accounting
             </Link>
-          </div>
-          <div
+            <Link to="/" 
             className={this.state.activeNav === "reports" ? "active" : ""}
             onClick={() => this.setActiveNav("reports")}
-          >
-            <i className="material-icons md-dark md-inactive">subject</i>
-            <Link to="/reports" className="md-dark md-inactive">
+            >
+              <i className="material-icons md-dark">subject</i>
               Reports
             </Link>
-          </div>
-          <div
+            <Link to="/" 
             className={this.state.activeNav === "connectedapps" ? "active" : ""}
             onClick={() => this.setActiveNav("connectedapps")}
-          >
-            <i className="material-icons md-dark md-inactive">device_hub</i>
-            <Link to="/connectedapps" className="md-dark md-inactive">
+            >
+              <i className="material-icons md-dark">device_hub</i>
               Connected Apps
             </Link>
-          </div>
-          <figure className="gap" />
-          <figure className="gap" />
-          <figure className="gap" />
-          <figure className="gap" />
-          <figure className="gap" />
-          <div
+            <figure className="gap" />
+            <figure className="gap" />
+            <figure className="gap" />
+            <figure className="gap" />
+            <figure className="gap" />
+            <Link to="/" 
             className={this.state.activeNav === "settings" ? "active" : ""}
             onClick={() => this.setActiveNav("settings")}
-          >
-            <i className="material-icons md-dark md-inactive">settings</i>
-            <Link to="/settings" className="md-dark md-inactive">
+            >
+              <i className="material-icons md-dark">settings</i>
               Settings
             </Link>
-          </div>
         </div>
-        {/* <div className="settings">
-                    <i className='material-icons md-dark md-inactive'>settings</i>
-                    <Link to="/settings" className='md-dark md-inactive'>Settings</Link>
-                </div> */}
       </div>
     );
   }
